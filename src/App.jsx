8a87c0b46258457d794419c513ea8cb9dc42c0d7
soongu <naong526@naver.com>
2025-02-03 11:54:48 +0900

    # 1-4-2. props 기초

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,21 @@ const App = () => {
 
   return (
     <>
-      <ExpenseItem />
+      <ExpenseItem
+        title={'닭강정'}
+        price={8000}
+        date={new Date(2025, 1, 3)}
+      />
+      <ExpenseItem
+        title={'아이스크림'}
+        price={5000}
+        date={new Date(2025, 2, 1)}
+      />
+      <ExpenseItem
+        title={'자바책 구매'}
+        price={30000}
+        date={new Date(2024, 12, 23)}
+      />
     </>
   );
 }
