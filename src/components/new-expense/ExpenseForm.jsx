5d259ba9ce588c0847d085ec12a9d87c4f8c41e9
soongu <naong526@naver.com>
2025-02-04 10:50:58 +0900

    # 2-3-2. 단일값으로 여러개 상태관리하기

--- src/components/new-expense/ExpenseForm.jsx
+++ src/components/new-expense/ExpenseForm.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+
+  // 상태값 관리
+  const [title, setTitle] = useState('');
+  const [price, setPrice] = useState(0);
+  const [date, setDate] = useState(null);
+
+
   // 오늘 날짜를 YYYY-MM-DD 형식으로 가져오는 함수
   const getTodayDate = () => {
     const today = new Date();
@@ -11,12 +18,30 @@ const ExpenseForm = () => {
     return `${year}-${month}-${day}`;
   };
 
+  // form submit 이벤트
+  const handleSubmit = e => { 
+    e.preventDefault();
+    // console.log('submit!!');
+
+    const payload = {
+      title,
+      price,
+      date
+    };
+
+    console.log('payload: ', payload);
+    
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' />
+          <input
+            type='text'
+            onInput={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Price</label>
@@ -24,6 +49,7 @@ const ExpenseForm = () => {
             type='number'
             min='100'
             step='100'
+            onInput={(e) => setPrice(+e.target.value)}
           />
         </div>
         <div className='new-expense__control'>
@@ -32,6 +58,7 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             max={getTodayDate()}
+            onInput={(e) => setDate(e.target.value)}
           />
         </div>
       </div>
