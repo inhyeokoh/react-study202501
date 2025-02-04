8bf68015d95134bf717d9d4be0761f3561e44e85
soongu <naong526@naver.com>
2025-02-04 09:38:47 +0900

    # 2-1-1. 이벤트 바인딩

--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -10,6 +10,11 @@ const ExpenseItem = ({ expense }) => {
   // 원화 표기법으로 변환
   const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
 
+  // 이벤트 핸들러 정의
+  function handleClick(e) {
+    console.log('click!!');
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate expenseDate={date} />
@@ -17,6 +22,18 @@ const ExpenseItem = ({ expense }) => {
         <h2>{title}</h2>
         <div className='expense-item__price'>{formatPrice}원</div>
       </div>
+
+      <button
+        id='btn1'
+        onClick={handleClick}>
+        버튼1
+      </button>
+      <button
+        id='btn2'
+        onMouseLeave={(e) => alert('나가기!')}
+        onMouseOver={(e) => alert('이벤트!!')}>
+        버튼2
+      </button>
     </Card>
   );
 };
