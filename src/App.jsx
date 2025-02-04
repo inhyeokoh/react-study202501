838c9a53e298c4d29b0bf48d620b5f17d82381c4
soongu <naong526@naver.com>
2025-02-04 11:20:04 +0900

    # 2-3-3. 상태관리 연습문제

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import ExpenseList from "./components/expenses/ExpenseList";
 import NewExpense from "./components/new-expense/NewExpense";
+import CheckBoxStyle from "./components/practice/CheckBoxStyle";
 
 // 컴포넌트
 const App = () => {
@@ -26,6 +27,7 @@ const App = () => {
 
   return (
     <>
+      <CheckBoxStyle />
       <NewExpense />
       <ExpenseList expenses={expenses} />
     </>
