2e5205df4ceb83ec24dcc25af4a98b2947d7c42b
soongu <naong526@naver.com>
2025-02-04 12:39:06 +0900

    # 2-4-2. 상향식 데이터 전달 연습문제

--- src/components/expenses/ExpenseList.jsx
+++ src/components/expenses/ExpenseList.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem';
 import './ExpenseList.css';
+import ExpenseFilter from './ExpenseFilter';
 
 const ExpenseList = ({ expenses }) => {
+
+  // ExpenseFilter에서 선택한 연도값을 여기서 출력
+  console.log('필터 연도값을 출력');
+  
   return (
     <div className='expenses'>
+      <ExpenseFilter />
       <ExpenseItem expense={expenses[0]} />
       <ExpenseItem expense={expenses[1]} />
       <ExpenseItem expense={expenses[2]} />
