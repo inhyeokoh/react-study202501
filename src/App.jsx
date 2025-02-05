9a554817779f2f19c4a4b60fe6f90dfcd2bd1007
soongu <naong526@naver.com>
2025-02-05 12:33:20 +0900

    # 종합복습 1-2. 목표 추가하기 기능

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import CourseInput from './components/CourseGoals/CourseInput';
 import CourseList from './components/CourseGoals/CourseList';
 
 // 컴포넌트
 const App = () => {
+
+  // 목표데이터들의 묶음배열
+  const [goals, setGoals] = useState([]);
+
+  // CourseInput에게 전달할 함수
+  const onAddGoal = (goal) => {
+    setGoals([ ...goals, goal ]);
+  };
   
   return (
     <div>
       <section id='goal-form'>
-        <CourseInput />
+        <CourseInput onAdd={onAddGoal} />
       </section>
       <section id='goals'>
-        <CourseList />
+        <CourseList items={goals} />
       </section>
     </div>
   );
