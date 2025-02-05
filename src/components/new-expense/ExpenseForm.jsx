346e0d74d360d60dde732f953e42bd9f8cd6179d
soongu <naong526@naver.com>
2025-02-05 10:43:46 +0900

    # 3-2-2. form expose 버튼 조건부 렏너링

--- src/components/new-expense/ExpenseForm.jsx
+++ src/components/new-expense/ExpenseForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './ExpenseForm.css';
 
-const ExpenseForm = ({ onAdd }) => {
+const ExpenseForm = ({ onAdd, onCancel }) => {
   
 
   // 단일 상태값 관리
@@ -114,6 +114,7 @@ const ExpenseForm = ({ onAdd }) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type='button' className='cancel-btn' onClick={onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
