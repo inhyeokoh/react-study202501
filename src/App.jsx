eadecbf077917ee911c18be0b39490239f3e07e3
soongu <naong526@naver.com>
2025-02-06 09:23:47 +0900

    # 종합복습 1-4. 삭제기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,38 @@ import CourseList from './components/CourseGoals/CourseList';
 const App = () => {
 
   // 목표데이터들의 묶음배열
-  const [goals, setGoals] = useState([]);
+  const [goals, setGoals] = useState([
+    {
+      id: 'g1',
+      text: '테스트 데이터1',
+    },
+    {
+      id: 'g2',
+      text: '테스트 데이터2',
+    },
+  ]);
 
   // CourseInput에게 전달할 함수
   const onAddGoal = (goal) => {
     setGoals([ ...goals, goal ]);
   };
 
-  console.log(goals);
-  
+  // console.log(goals);
+
+  // 삭제를 위한 함수
+  const onDeleteGoal = (id) => { 
+    // id를 통해 해당 객체를 탐색한 후 배열에서 제거
+    
+    setGoals(goals.filter((g) => g.id !== id));
+
+    // for (let i = 0; i < goals.length; i++) {
+    //   if (goals[i].id === id) {
+    //     const copyGoals = [...goals];
+    //     copyGoals.splice(i, 1);
+    //     setGoals(copyGoals);
+    //   }
+    // }
+  };
   
   return (
     <div>
@@ -23,7 +46,7 @@ const App = () => {
         <CourseInput onAdd={onAddGoal} />
       </section>
       <section id='goals'>
-        <CourseList items={goals} />
+        <CourseList items={goals} onDelete={onDeleteGoal} />
       </section>
     </div>
   );
--- src/components/CourseGoals/CourseItem.jsx
+++ src/components/CourseGoals/CourseItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './CourseItem.css';
 
-const CourseItem = ({ item }) => {
+const CourseItem = ({ item, onDelete }) => {
 
   const { id, text } = item;
   
@@ -11,8 +11,13 @@ const CourseItem = ({ item }) => {
    3. 클릭한 아이템을 App.jsx가 찾게하기위해 자신의 id값을 App.jsx로 끌어올려보냄
    4. App.jsx에서는 받은 id를 기반으로 goals배열에서 해당 아이템을 제거후 상태변경을 통해 리렌더링
   */
+  
+  const handleDelete = e => { 
+    // console.log('삭제!! ', id);
+    onDelete(id);
+  };
 
-  return <li className='goal-item'>{text}</li>;
+  return <li className='goal-item' onClick={handleDelete}>{text}</li>;
 };
 
 export default CourseItem;
