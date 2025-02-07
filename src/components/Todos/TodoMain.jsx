c267436a3d7f868e093db92427e618a2f0908385
soongu <naong526@naver.com>
2025-02-07 09:42:38 +0900

    # 종합복습 2-4. 할 일 추가 기능

--- src/components/Todos/TodoMain.jsx
+++ src/components/Todos/TodoMain.jsx
@@ -4,19 +4,12 @@ import styles from './scss/TodoMain.module.scss';
 import TodoItem from './TodoItem';
 
 
-const DUMMY_TODOS = [
-  { id: 1, title: '리액트 공부', done: true },
-  { id: 2, title: '점심 먹기', done: false },
-  { id: 3, title: '프로젝트하기', done: false },
-  { id: 4, title: '숙제하기', done: true },
-];
-
 // 컴포넌트
-const TodoMain = () => {
+const TodoMain = ({items}) => {
   
   return (
     <ul className={styles['todo-list']}>
-      {DUMMY_TODOS.map((todo) => (
+      {items.map((todo) => (
         <TodoItem key={todo.id} item={todo} />
       ))}
     </ul>
--- src/components/Todos/TodoTemplate.jsx
+++ src/components/Todos/TodoTemplate.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoHeader from './TodoHeader';
 import styles from './scss/TodoTemplate.module.scss';
 import TodoMain from './TodoMain';
 import TodoInput from './TodoInput';
 
+const DUMMY_TODOS = [
+  { id: 1, title: '리액트 공부', done: true },
+  { id: 2, title: '점심 먹기', done: false },
+  { id: 3, title: '프로젝트하기', done: false },
+  { id: 4, title: '숙제하기', done: true },
+];
+
 const TodoTemplate = () => {
+
+  // 할일 목록을 상태관리
+  const [todoList, setTodoList] = useState(DUMMY_TODOS);
+
+  // 데이터 상향식 전달을 위한 할 일 추가 함수 내려주기
+  const addTodo = (newTitle) => { 
+    const newTodo = {
+      id: Math.random().toString()
+      , title: newTitle
+      , done: false
+    };
+
+    setTodoList([...todoList, newTodo]);
+  };
+
   return (
     <div className={styles.TodoTemplate}>
       <TodoHeader />
-      <TodoMain />
-      <TodoInput />
+      <TodoMain items={todoList} />
+      <TodoInput onAdd={addTodo} />
     </div>
   );
 };
