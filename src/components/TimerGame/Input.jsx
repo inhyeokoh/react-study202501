9cc83b5d8435f97e16374715d9a4b866e384ed2d
soongu <naong526@naver.com>
2025-02-11 09:48:02 +0900

    # 종합복습 3-7. forwardRef로 Input컴포넌트 제어

---
+++ src/components/TimerGame/Input.jsx
@@ -0,0 +1,9 @@
+import React, { forwardRef } from 'react';
+
+const InputComponent = (props, ref) => {
+  return <input type={props.type} ref={ref} />;
+};
+
+const Input = forwardRef(InputComponent);
+
+export default Input;
